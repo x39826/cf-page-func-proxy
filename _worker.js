7b00fd5643fc667d6031f34f0c50b48aa6b5f0e3
HuJiawei <x39826@users.noreply.github.com>
2024-12-20 22:18:21 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="example.com";
+        url.hostname="jwhu2-baoku-20241220.cpolar.io";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
